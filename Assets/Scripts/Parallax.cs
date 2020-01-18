b178f3535d62a6fb104ec1126b2ca9c269d35ba4
Bilas Yurii <bilas.yurii@gmail.com>
2020-01-18 14:23:12 +0200

    Fixed jagging and added background parallax effect.

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float length, startPos;
+
+    public GameObject cam;
+    public float parallaxCoefficient;
+
+    private void Awake()
+    {
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    private void FixedUpdate()
+    {
+        float temp = cam.transform.position.x * (1f - parallaxCoefficient);
+
+        float distance = (cam.transform.position.x * parallaxCoefficient);
+
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+
+        if (temp > startPos + length)
+        {
+            startPos += length;
+        }
+        else if (temp < startPos - length)
+        {
+            startPos -= length;
+        }
+    }
+}
